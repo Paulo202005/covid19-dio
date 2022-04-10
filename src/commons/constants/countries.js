473b1671252ed7c53b0f9130952e89fc48b5f4d4
Paulo Argenton <paulo.argenton@gmail.com>
2022-04-10 12:16:41 -0300

    Projeto Covid19 - Desafio da DIO

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,71 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import JapanFlag from '../../assets/images/japan.jpeg'
+import WorldFlag from '../../assets/images/world.jpeg'
+import EnglandFlag from '../../assets/images/england.png'
+import FranceFlag from '../../assets/images/france.jpeg'
+import SpainFlag from '../../assets/images/spain.png'
+import PortugalFlag from '../../assets/images/portugal.png'
+
+const COUNTRIES = [
+  {
+    label: 'Mundo',
+    value: 'world',
+    flag: WorldFlag
+  },
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Espanha',
+    value: 'spain',
+    flag: SpainFlag
+  },  
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'França',
+    value: 'france',
+    flag: FranceFlag
+  },
+  {
+    label: 'Inglaterra',
+    value: 'UK',
+    flag: EnglandFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'Japão',
+    value: 'japan',
+    flag: JapanFlag
+  },
+  {
+    label: 'Portugal',
+    value: 'portugal',
+    flag: PortugalFlag
+  }
+]
+
+export default COUNTRIES
